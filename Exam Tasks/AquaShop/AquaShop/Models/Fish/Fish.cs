6de5578a69763a7066faec20079fd2707a3481d5
Desislava <106395233+Desislava26@users.noreply.github.com>
2023-01-17 17:32:58 +0200

    Add files via upload

---
+++ Exam Tasks/AquaShop/AquaShop/Models/Fish/Fish.cs
@@ -0,0 +1,75 @@
+﻿using AquaShop.Models.Fish.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Fish
+{
+    public abstract class Fish : IFish
+    {
+        protected Fish(string name, string species, decimal price)
+        {
+            Name = name;
+            Species = species;
+            Price = price;
+
+        }
+        private string name;
+        public string Name 
+        {
+            get => name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Fish name cannot be null or empty.");
+                }
+                name = value;
+            }
+        }
+        private string species;
+        public string Species
+        {
+            get => species;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Fish species cannot be null or empty.");
+                }
+                species = value;
+            }
+        }
+
+        private int size;
+        public int Size
+        {
+            get => size;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid Size");
+                }
+                size = value;
+            }
+        }
+
+        private decimal price;
+        public decimal Price
+        {
+            get => price;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Fish price cannot be below or equal to 0.");
+                }
+                price = value;
+            }
+        }
+
+        public abstract void Eat();
+        
+    }
+}
---
+++ Exam Tasks/BookingApp/Models/Rooms/Room.cs
@@ -0,0 +1,53 @@
+﻿using BookingApp.Models.Rooms.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Rooms
+{
+    public abstract class Room : IRoom
+    {
+        protected Room(int bedCapacity)
+        {
+            BedCapacity = bedCapacity;
+            
+        }
+
+        private int bedCapacity;
+        public int BedCapacity
+        {
+            get => bedCapacity;
+             set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Invalid");
+                }
+
+                bedCapacity = value;
+            }
+        }
+
+        private double priceForNight = 0;
+        public double PricePerNight
+        {
+            get => priceForNight;
+             set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Price cannot be negative!");
+                }
+                
+                priceForNight = value;
+            }
+        }
+        //private bool isItSetted = false;
+        public bool IsItSetted { get; set ; } = false;
+        public void SetPrice(double price)
+        {
+            this.PricePerNight = price;
+            
+        }
+    }
+}
